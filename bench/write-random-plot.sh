4fd1c5b65533e5850274c914bcebad0352c819f2
Rod Vagg <rod@vagg.org>
2013-06-09 20:48:50 +1000

    add random writes bench

--- bench/write-random-plot.sh
+++ bench/write-random-plot.sh
@@ -16,9 +16,8 @@ gnuplot <<EOF
   set key below
   set grid
 
+<<<<<<< HEAD
   plot "write_random_times_g32.csv" using (\$1/1000):(\$2/1000000) title "Google LevelDB" lc rgb "red" lt 7 ps 0.3, \
        "write_random_times_h32.csv" using (\$1/1000):(\$2/1000000) title "HyperDex LevelDB" lc rgb "green" lt 7 ps 0.3, \
        "write_random_times_b32.csv" using (\$1/1000):(\$2/1000000) title "Basho LevelDB" lc rgb "blue" lt 7 ps 0.3, \
        "write_random_times.csv" using (\$1/1000):(\$2/1000000) title "LMDB" lc rgb "black" lt 7 ps 0.3
-
-EOF
