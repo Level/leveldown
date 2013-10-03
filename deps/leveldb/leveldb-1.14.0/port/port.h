09af2a750920b106f3abedc9a98fa438f359624c
Rod Vagg <rod@vagg.org>
2013-10-03 16:28:59 +1000

    added port_uv.h reference

--- deps/leveldb/leveldb-1.14.0/port/port.h
+++ deps/leveldb/leveldb-1.14.0/port/port.h
@@ -10,7 +10,9 @@
 // Include the appropriate platform specific file below.  If you are
 // porting to a new platform, see "port_example.h" for documentation
 // of what the new port_<platform>.h file must provide.
-#if defined(LEVELDB_PLATFORM_POSIX)
+#if defined(LEVELDB_PLATFORM_UV)
+#  include "port_uv.h"
+#elif defined(LEVELDB_PLATFORM_POSIX)
 #  include "port/port_posix.h"
 #elif defined(LEVELDB_PLATFORM_CHROMIUM)
 #  include "port/port_chromium.h"
