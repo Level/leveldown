dde38cf3b1c6bb194d309192b6d69a9b07445c2b
Rod Vagg <rod@vagg.org>
2013-11-21 15:50:01 +1100

    update rocksdb, nan@0.6.0

--- deps/leveldb/leveldb-rocksdb/util/build_version.cc
+++ deps/leveldb/leveldb-rocksdb/util/build_version.cc
@@ -1,11 +1,6 @@
 #include "build_version.h"
 
-<<<<<<< HEAD
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:5c24e7e3091105d34a654117a2cb6a845dc644d7";
-const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Tue 19 Nov 2013 14:09:46 EST";
-=======
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:a0ce3fd00a1ae35bcbb9edc603b7398d83a8eb23";
-const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Sat Nov 16 21:47:00 EST 2013";
->>>>>>> added rocksdb source
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:56589ab81f6827ff7402e31b24a6d548f29a524f";
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Thu Nov 21 15:45:16 EST 2013";
 const char* rocksdb_build_compile_date = __DATE__;
 const char* rocksdb_build_compile_time = __TIME__;
