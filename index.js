71ac0ff3b0f0a0e62b7b7e875fb8db5c721cb7aa
David Björklund <david.bjorklund@gmail.com>
2014-05-12 18:26:53 +0200

    wrap iterator in a javascript-function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('bindings')('leveldown.node').leveldown
+var binding = require('bindings')('leveldown.node').leveldown
+  , wrapper = function (options) {
+      var instance = binding(options)
+
+      instance.iterator = require('./lib/iterator')(instance)
+
+      return instance
+    }
+
+wrapper.destroy = binding.destroy
+wrapper.repair = binding.repair
+
+module.exports = wrapper
