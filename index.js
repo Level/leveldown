c6b4f90ed6c4ecdc2ce0c777fb36a87cbd1b73aa
David Björklund <david.bjorklund@gmail.com>
2014-05-12 23:00:05 +0200

    cleanup

--- index.js
+++ index.js
@@ -2,53 +2,53 @@ var binding = require('bindings')('leveldown.node').leveldown
 
   , Iterator = require('./lib/iterator')
 
-  , LevelDown = function (options) {
-      if (!(this instanceof LevelDown))
-        return new LevelDown(options)
+  , LevelDOWN = function (location) {
+      if (!(this instanceof LevelDOWN))
+        return new LevelDOWN(location)
 
-      this.binding = binding(options)
+      this.binding = binding(location)
     }
 
-LevelDown.prototype.open = function (options, callback) {
+LevelDOWN.prototype.open = function (options, callback) {
   this.binding.open(options, callback)
 }
 
-LevelDown.prototype.close = function (callback) {
+LevelDOWN.prototype.close = function (callback) {
   this.binding.close(callback)
 }
 
-LevelDown.prototype.batch = function (operations, options, callback) {
+LevelDOWN.prototype.put = function (key, value, options, callback) {
+  this.binding.put(key, value, options, callback)
+}
+
+LevelDOWN.prototype.get = function (key, options, callback) {
+  this.binding.get(key, options, callback)
+}
+
+LevelDOWN.prototype.del = function (key, options, callback) {
+  this.binding.del(key, options, callback)
+}
+
+LevelDOWN.prototype.batch = function (operations, options, callback) {
   if (arguments.length === 0)
     return this.binding.batch()
 
   return this.binding.batch(operations, options, callback)
 }
 
-LevelDown.prototype.get = function (key, options, callback) {
-  this.binding.get(key, options, callback)
-}
-
-LevelDown.prototype.getProperty = function (property) {
-  return this.binding.getProperty(property)
-}
-
-LevelDown.prototype.del = function (key, options, callback) {
-  this.binding.del(key, options, callback)
-}
-
-LevelDown.prototype.put = function (key, value, options, callback) {
-  this.binding.put(key, value, options, callback)
-}
-
-LevelDown.prototype.approximateSize = function (start, end, callback) {
+LevelDOWN.prototype.approximateSize = function (start, end, callback) {
   this.binding.approximateSize(start, end, callback)
 }
 
-LevelDown.prototype.iterator = function (options) {
+LevelDOWN.prototype.getProperty = function (property) {
+  return this.binding.getProperty(property)
+}
+
+LevelDOWN.prototype.iterator = function (options) {
   return new Iterator(this.binding, options)
 }
 
-LevelDown.destroy = binding.destroy
-LevelDown.repair = binding.repair
+LevelDOWN.destroy = binding.destroy
+LevelDOWN.repair = binding.repair
 
-module.exports = LevelDown
+module.exports = LevelDOWN
