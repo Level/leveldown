4ac578db555cb905e31e8e6ecbca361871515bda
David Björklund <david.bjorklund@gmail.com>
2014-05-12 23:29:46 +0200

    inherit from AbstractLevelDOWN & AbstractIterator

--- index.js
+++ index.js
@@ -15,34 +15,34 @@ var util = require('util')
 
 util.inherits(LevelDOWN, AbstractLevelDOWN)
 
-LevelDOWN.prototype.open = function (options, callback) {
+LevelDOWN.prototype._open = function (options, callback) {
   this.binding.open(options, callback)
 }
 
-LevelDOWN.prototype.close = function (callback) {
+LevelDOWN.prototype._close = function (callback) {
   this.binding.close(callback)
 }
 
-LevelDOWN.prototype.put = function (key, value, options, callback) {
+LevelDOWN.prototype._put = function (key, value, options, callback) {
   this.binding.put(key, value, options, callback)
 }
 
-LevelDOWN.prototype.get = function (key, options, callback) {
+LevelDOWN.prototype._get = function (key, options, callback) {
   this.binding.get(key, options, callback)
 }
 
-LevelDOWN.prototype.del = function (key, options, callback) {
+LevelDOWN.prototype._del = function (key, options, callback) {
   this.binding.del(key, options, callback)
 }
 
-LevelDOWN.prototype.batch = function (operations, options, callback) {
+LevelDOWN.prototype._batch = function (operations, options, callback) {
   if (arguments.length === 0)
     return this.binding.batch()
 
   return this.binding.batch(operations, options, callback)
 }
 
-LevelDOWN.prototype.approximateSize = function (start, end, callback) {
+LevelDOWN.prototype._approximateSize = function (start, end, callback) {
   this.binding.approximateSize(start, end, callback)
 }
 
@@ -50,7 +50,7 @@ LevelDOWN.prototype.getProperty = function (property) {
   return this.binding.getProperty(property)
 }
 
-LevelDOWN.prototype.iterator = function (options) {
+LevelDOWN.prototype._iterator = function (options) {
   return new Iterator(this.binding, options)
 }
 
