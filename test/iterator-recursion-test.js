85e6a38fed5275e4411f2412f9fe80d84c7c8a70
Adam Bliss <abliss@gmail.com>
2014-06-16 13:44:09 +0000

    Test and fix for issue 109

--- test/iterator-recursion-test.js
+++ test/iterator-recursion-test.js
@@ -1,6 +1,7 @@
-const test       = require('tap').test
-    , testCommon = require('abstract-leveldown/testCommon')
-    , leveldown  = require('../')
+const test          = require('tap').test
+    , testCommon    = require('abstract-leveldown/testCommon')
+    , leveldown     = require('../')
+    , child_process = require('child_process') 
 
 var db
   , sourceData = (function () {
@@ -25,6 +26,26 @@ test('setUp db', function (t) {
   db.open(function () {
     db.batch(sourceData, t.end.bind(t))
   })
+})
+
+test('try to create an iterator with a blown stack', function (t) {
+  // Reducing the stack size down from the default 984 for the child node
+  // process makes it easier to trigger the bug condition. But making it too low
+  // causes the child process to die for other reasons.
+  var opts  = { execArgv: ["-stack-size=128"] }
+  ,   child = child_process.fork('stack-blower.js', ["run"], opts)
+  
+  child.on('message', function (m) {
+      t.ok(true, m)
+      child.disconnect()
+      
+      t.end()
+    })
+    .on('exit', function (code, sig) {
+      t.ok(false, "Child exited with code=" + code + " sig=" + sig)
+
+      t.end()
+    })
 })
 
 test('iterate over a large iterator with a large watermark', function (t) {
