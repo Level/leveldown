e2ecdf7a2659f8df1c4c844ecd6da045e673e3da
Riceball LEE <snowyu.lee@gmail.com>
2014-11-23 12:53:44 +0800

    + UINT32_MAX ifndef this for travisCI

--- deps/leveldb/leveldb-1.18/port/port.h
+++ deps/leveldb/leveldb-1.18/port/port.h
@@ -18,4 +18,12 @@
 #  include "port/port_chromium.h"
 #endif
 
+#ifndef UINT32_MAX
+#  define UINT32_MAX   0xffffffff
+#endif
+
+#ifndef SIZE_MAX
+#  define SIZE_MAX     UINT32_MAX
+#endif
+
 #endif  // STORAGE_LEVELDB_PORT_PORT_H_
