0c8e59ed08f1450a210e0168a1a082feebc64ac5
Riceball LEE <snowyu.lee@gmail.com>
2014-12-01 10:42:23 +0800

    + using the new snowyu/abstract-leveldown#feature/sync to test

--- test/del-test.js
+++ test/del-test.js
@@ -3,41 +3,5 @@ const test       = require('tap').test
     , leveldown  = require('../')
     , abstract   = require('abstract-leveldown/abstract/del-test')
 
-var db
-    ,isTypedArray        = require('abstract-leveldown/abstract/util').isTypedArray
-
-if (require.main === module) {
-  var oldTearDown = abstract.tearDown
-  abstract.tearDown = function (test, testCommon) {
-    oldTearDown(test, testCommon)
-    test('tearDown', function (t) {
-      db.close(testCommon.tearDown.bind(null, t))
-    })
-  }
-  abstract.args = function (test) {
-    test('setUp common', testCommon.setUp)
-    test('setUp db', function (t) {
-      db = leveldown(testCommon.location())
-      db.open(t.end.bind(t))
-    })
-    test('test argument-less del() throws', function (t) {
-      t.throws(
-          db.del.bind(db)
-        , { name: 'Error', message: 'key cannot be `null` or `undefined`' }
-        , 'no-arg del() throws'
-      )
-      t.end()
-    })
-
-    test('test callback-less, 1-arg, del() sync', function (t) {
-      t.ok(db.del('foo'), "should delete key")
-      t.end()
-    })
-
-    test('test callback-less, 3-arg, del() sync', function (t) {
-      t.ok(db.del('foo', {}), "should delete key with empty options")
-      t.end()
-    })
-  }
+if (require.main === module)
   abstract.all(leveldown, test, testCommon)
-}
--- test/open-test.js
+++ test/open-test.js
@@ -3,28 +3,5 @@ const test       = require('tap').test
     , leveldown  = require('../')
     , abstract   = require('abstract-leveldown/abstract/open-test')
 
-var db
-    ,isTypedArray        = require('abstract-leveldown/abstract/util').isTypedArray
-
-if (require.main === module) {
-  abstract.args = function (test) {
-    test('setUp common', testCommon.setUp)
-    test('setUp db', function (t) {
-      db = leveldown(testCommon.location())
-      db.open(t.end.bind(t))
-    })
-    test('test database open no-arg sync', function (t) {
-      var db = leveldown(testCommon.location())
-      t.ok(db.open(), "should be ok")
-      t.end()
-    })
-
-    test('test callback-less, 1-arg, open() sync', function (t) {
-      var db = leveldown(testCommon.location())
-      t.ok(db.open({}), "should be ok")
-      t.end()
-      t.end()
-    })
-  }
+if (require.main === module)
   abstract.all(leveldown, test, testCommon)
-}
