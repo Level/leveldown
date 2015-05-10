9ef4894dea4bc043456e0c587d946dbbef9fd0c4
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-05-10 14:07:46 +0200

    add tests for checking backup folder

--- test/livebackup-test.js
+++ test/livebackup-test.js
@@ -1,13 +1,17 @@
 const test       = require('tap').test
     , testCommon = require('abstract-leveldown/testCommon')
     , leveldown  = require('../')
+    , fs         = require('fs')
+    , join       = require('path').join
 
 var db
+  , location
 
 test('setUp common', testCommon.setUp)
 
 test('setUp db', function (t) {
-  db = leveldown(testCommon.location())
+  location = testCommon.location()
+  db = leveldown(location)
   db.open(function (err) {
     t.ok(!err, 'no error')
     var key = 'beep'
@@ -23,22 +27,26 @@ test('setUp db', function (t) {
 })
 
 test('liveBackup', function (t) {
-  t.throws(db.liveBackup.bind(db, null), {
+  var expected = {
     name: 'Error',
     message: 'liveBackup() requires a valid `name` argument'
-  })
-  t.throws(db.liveBackup.bind(db), {
-    name: 'Error',
-    message: 'liveBackup() requires a valid `name` argument'
-  })
-  t.throws(db.liveBackup.bind(db, function () {}), {
-    name: 'Error',
-    message: 'liveBackup() requires a valid `name` argument'
-  })
-  var backup = 'backup-' + Date.now()
-  db.liveBackup(backup, function (err) {
+  }
+  t.throws(db.liveBackup.bind(db, null), expected)
+  t.throws(db.liveBackup.bind(db), expected)
+  t.throws(db.liveBackup.bind(db, function () {}), expected)
+  var now = Date.now()
+  db.liveBackup(now, function (err) {
     t.ok(!err, 'no liveBackup error')
-    t.end()
+    var backup = join(location, 'backup-' + now)
+    fs.stat(backup, function (err, stats) {
+      t.ok(!err, 'no error')
+      t.ok(stats.isDirectory(), 'backup dir should exist')
+      fs.readdir(backup, function (err, files) {
+        t.ok(!err, 'no error')
+        t.ok(files.length > 0, 'should contain some files')
+        t.end()
+      })
+    })
   })
 })
 
