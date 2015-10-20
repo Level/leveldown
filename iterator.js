7d85c7405709fb9133de124918432bf4c5f32ecb
Tim Kuijsten <tim@netsend.nl>
2015-10-20 19:13:43 +0200

    require at start, not later
    chrooted programs can only require at startup, before being chrooted

--- iterator.js
+++ iterator.js
@@ -1,5 +1,6 @@
 const util             = require('util')
     , AbstractIterator = require('abstract-leveldown').AbstractIterator
+    , fastFuture       = require('fast-future')
 
 
 function Iterator (db, options) {
@@ -8,7 +9,7 @@ function Iterator (db, options) {
   this.binding    = db.binding.iterator(options)
   this.cache      = null
   this.finished   = false
-  this.fastFuture = require('fast-future')()
+  this.fastFuture = fastFuture()
 }
 
 util.inherits(Iterator, AbstractIterator)
