7e806d84336053a407b4ccbe4720a4ebb2d04333
Vincent Weevers <mail@vincentweevers.nl>
2016-02-15 15:12:40 +0100

    cleanup and code style

--- test/iterator-test.js
+++ test/iterator-test.js
@@ -10,12 +10,19 @@ const test       = require('tape')
 abstract.all(leveldown, test, testCommon)
 
 make('iterator seek throws if key is not a string or buffer', function (db, t, done) {
-  var keys = [void 0, 10, true, null]
+  var keys = [undefined, 10, true, null]
   var pending = keys.length
 
-  keys.forEach(function(key){
-    var ite = db.iterator(), error;
-    try { ite.seek(key) } catch (e) { error = e }
+  keys.forEach(function (key) {
+    var error
+    var ite = db.iterator()
+
+    try {
+      ite.seek(key)
+    } catch (e) {
+      error = e
+    }
+
     t.ok(error, 'had error from seek()')
     ite.end(end)
   })
@@ -30,11 +37,11 @@ make('iterator is seekable', function (db, t, done) {
   var ite = db.iterator()
   ite.seek('two')
   ite.next(function (err, key, value) {
-    t.error(err, 'no error from next()')
+    t.error(err, 'no error')
     t.same(key.toString(), 'two', 'key matches')
     t.same(value.toString(), '2', 'value matches')
     ite.next(function (err, key, value) {
-      t.error(err, 'no error from next()')
+      t.error(err, 'no error')
       t.same(key, undefined, 'end of iterator')
       t.same(value, undefined, 'end of iterator')
       ite.end(done)
@@ -42,130 +49,14 @@ make('iterator is seekable', function (db, t, done) {
   })
 })
 
-make('iterator is seekable with buffer', function (db, t, done) {
-  var ite = db.iterator()
-  ite.seek(new Buffer('two'))
-  ite.next(function (err, key, value) {
-    t.error(err, 'no error from next()')
-    t.same(key.toString(), 'two', 'key matches')
-    t.same(value.toString(), '2', 'value matches')
-    ite.next(function (err, key, value) {
-      t.error(err, 'no error from next()')
-      t.same(key, undefined, 'end of iterator')
-      t.same(value, undefined, 'end of iterator')
-      ite.end(done)
-    })
-  })
-})
-
-make('iterator reverse seek in the middle', function (db, t, done) {
+make('reverse seek in the middle', function (db, t, done) {
   var ite = db.iterator({reverse: true, limit: 1})
   ite.seek('three!')
   ite.next(function (err, key, value) {
-    t.error(err, 'no error from next()')
+    t.error(err, 'no error')
     t.same(key.toString(), 'three', 'key matches')
     t.same(value.toString(), '3', 'value matches')
     ite.end(done)
-  })
-})
-
-make('iterator seek resets state', function(db, t, done) {
-  var ite = db.iterator()
-  ite.next(function (err, key, value) {
-    t.error(err, 'no error from next()')
-    t.same(key.toString(), 'one', 'key matches')
-    t.ok(ite.cache, 'has cached items')
-    t.is(ite.finished, true, 'finished')
-    ite.seek('two')
-    t.notOk(ite.cache, 'cache is removed')
-    t.is(ite.finished, false, 'resets finished state')
-    done()
-  })
-})
-
-make('iterator seeks', function (db, t, done) {
-  db.batch(pairs(10, { not: 7 }), function(err){
-    t.error(err, 'no error from batch()')
-
-    var ite = db.iterator({gte: '4'})
-    ite.seek('5')
-    ite.next(function (err, key, value) {
-      t.error(err, 'no error from next()')
-      t.same(key.toString(), '5', 'key matches')
-      t.same(value.toString(), '5', 'value matches')
-      ite.seek('7')
-      ite.next(function (err, key, value) {
-        t.error(err, 'no error from next()')
-        t.same(key && key.toString(), '8', 'key matches')
-        t.same(value && value.toString(), '8', 'value matches')
-        ite.end(done)
-      })
-    })
-  })
-})
-
-make('iterator seeks without cache', function (db, t, done) {
-  db.batch(pairs(10, { not: 7 }), function(err){
-    t.error(err, 'no error from batch()')
-
-    var ite = db.iterator({gte: '4', highWaterMark: 1})
-    ite.seek('5')
-    ite.next(function (err, key, value) {
-      t.error(err, 'no error from next()')
-      t.is(ite.cache.length, 0, 'nothing cached')
-      t.same(key.toString(), '5', 'key matches')
-      t.same(value.toString(), '5', 'value matches')
-      ite.seek('7')
-      ite.next(function (err, key, value) {
-        t.error(err, 'no error from next()')
-        t.is(ite.cache.length, 0, 'nothing cached')
-        t.same(key && key.toString(), '8', 'key matches')
-        t.same(value && value.toString(), '8', 'value matches')
-        ite.end(done)
-      })
-    })
-  })
-})
-
-make('iterator reverse seeks', function (db, t, done) {
-  db.batch(pairs(10, { not: 5 }), function(err){
-    t.error(err, 'no error from batch()')
-
-    var ite = db.iterator({reverse: true, lte: '8'})
-    ite.next(function (err, key, value) {
-      t.error(err, 'no error from next()')
-      t.same(key.toString(), '8', 'key matches')
-      t.same(value.toString(), '8', 'value matches')
-      ite.seek('5')
-      ite.next(function (err, key, value) {
-        t.error(err, 'no error from next()')
-        t.same(key.toString(), '4', 'key matches')
-        t.same(value.toString(), '4', 'value matches')
-        ite.end(done)
-      })
-    })
-  })
-})
-
-make('iterator reverse seeks without cache', function (db, t, done) {
-  db.batch(pairs(10, { not: 5 }), function(err){
-    t.error(err, 'no error from batch()')
-
-    var ite = db.iterator({reverse: true, lte: '8', highWaterMark: 1})
-    ite.next(function (err, key, value) {
-      t.error(err, 'no error from next()')
-      t.is(ite.cache.length, 0, 'nothing cached')
-      t.same(key.toString(), '8', 'key matches')
-      t.same(value.toString(), '8', 'value matches')
-      ite.seek('5')
-      ite.next(function (err, key, value) {
-        t.error(err, 'no error from next()')
-        t.is(ite.cache.length, 0, 'nothing cached')
-        t.same(key.toString(), '4', 'key matches')
-        t.same(value.toString(), '4', 'value matches')
-        ite.end(done)
-      })
-    })
   })
 })
 
@@ -173,31 +64,147 @@ make('iterator invalid seek', function (db, t, done) {
   var ite = db.iterator()
   ite.seek('zzz')
   ite.next(function (err, key, value) {
-    t.error(err, 'no error from next()')
+    t.error(err, 'no error')
     t.same(key, undefined, 'end of iterator')
     t.same(value, undefined, 'end of iterator')
     ite.end(done)
   })
 })
 
-make('iterator reverse seek from invalid range', function (db, t, done) {
+make('reverse seek from invalid range', function (db, t, done) {
   var ite = db.iterator({reverse: true})
   ite.seek('zzz')
   ite.next(function (err, key, value) {
-    t.error(err, 'no error from next()')
+    t.error(err, 'no error')
     t.same(key.toString(), 'two', 'end of iterator')
     t.same(value.toString(), '2', 'end of iterator')
     ite.end(done)
   })
 })
 
-make('iterator seek lands on or after target', function(db, t, done) {
+make('iterator is seekable with buffer', function (db, t, done) {
+  var ite = db.iterator()
+  ite.seek(Buffer('two'))
+  ite.next(function (err, key, value) {
+    t.error(err, 'no error from next()')
+    t.equal(key.toString(), 'two', 'key matches')
+    t.equal(value.toString(), '2', 'value matches')
+    ite.next(function (err, key, value) {
+      t.error(err, 'no error from next()')
+      t.equal(key, undefined, 'end of iterator')
+      t.equal(value, undefined, 'end of iterator')
+      ite.end(done)
+    })
+  })
+})
+
+make('iterator seek resets state', function (db, t, done) {
+  var ite = db.iterator()
+  ite.next(function (err, key, value) {
+    t.error(err, 'no error from next()')
+    t.equal(key.toString(), 'one', 'key matches')
+    t.ok(ite.cache, 'has cached items')
+    t.equal(ite.finished, true, 'finished')
+    ite.seek('two')
+    t.notOk(ite.cache, 'cache is removed')
+    t.equal(ite.finished, false, 'resets finished state')
+    done()
+  })
+})
+
+make('iterator seeks', function (db, t, done) {
+  db.batch(pairs(10, { not: 7 }), function (err) {
+    t.error(err, 'no error from batch()')
+
+    var ite = db.iterator({gte: '4'})
+    ite.seek('5')
+    ite.next(function (err, key, value) {
+      t.error(err, 'no error from next()')
+      t.equal(key.toString(), '5', 'key matches')
+      t.equal(value.toString(), '5', 'value matches')
+      ite.seek('7')
+      ite.next(function (err, key, value) {
+        t.error(err, 'no error from next()')
+        t.equal(key.toString(), '8', 'key matches')
+        t.equal(value.toString(), '8', 'value matches')
+        ite.end(done)
+      })
+    })
+  })
+})
+
+make('iterator seeks without cache', function (db, t, done) {
+  db.batch(pairs(10, { not: 7 }), function (err) {
+    t.error(err, 'no error from batch()')
+
+    var ite = db.iterator({gte: '4', highWaterMark: 1})
+    ite.seek('5')
+    ite.next(function (err, key, value) {
+      t.error(err, 'no error from next()')
+      t.equal(ite.cache.length, 0, 'nothing cached')
+      t.equal(key.toString(), '5', 'key matches')
+      t.equal(value.toString(), '5', 'value matches')
+      ite.seek('7')
+      ite.next(function (err, key, value) {
+        t.error(err, 'no error from next()')
+        t.equal(ite.cache.length, 0, 'nothing cached')
+        t.equal(key.toString(), '8', 'key matches')
+        t.equal(value.toString(), '8', 'value matches')
+        ite.end(done)
+      })
+    })
+  })
+})
+
+make('iterator reverse seeks', function (db, t, done) {
+  db.batch(pairs(10, { not: 5 }), function (err) {
+    t.error(err, 'no error from batch()')
+
+    var ite = db.iterator({reverse: true, lte: '8'})
+    ite.next(function (err, key, value) {
+      t.error(err, 'no error from next()')
+      t.equal(key.toString(), '8', 'key matches')
+      t.equal(value.toString(), '8', 'value matches')
+      ite.seek('5')
+      ite.next(function (err, key, value) {
+        t.error(err, 'no error from next()')
+        t.equal(key.toString(), '4', 'key matches')
+        t.equal(value.toString(), '4', 'value matches')
+        ite.end(done)
+      })
+    })
+  })
+})
+
+make('iterator reverse seeks without cache', function (db, t, done) {
+  db.batch(pairs(10, { not: 5 }), function (err) {
+    t.error(err, 'no error from batch()')
+
+    var ite = db.iterator({reverse: true, lte: '8', highWaterMark: 1})
+    ite.next(function (err, key, value) {
+      t.error(err, 'no error from next()')
+      t.equal(ite.cache.length, 0, 'nothing cached')
+      t.equal(key.toString(), '8', 'key matches')
+      t.equal(value.toString(), '8', 'value matches')
+      ite.seek('5')
+      ite.next(function (err, key, value) {
+        t.error(err, 'no error from next()')
+        t.equal(ite.cache.length, 0, 'nothing cached')
+        t.equal(key.toString(), '4', 'key matches')
+        t.equal(value.toString(), '4', 'value matches')
+        ite.end(done)
+      })
+    })
+  })
+})
+
+make('iterator seek lands on or after target', function (db, t, done) {
   var max = 100, step = 15
-  db.batch(pairs(max, {lex: true, not: even}), function(err){
+  db.batch(pairs(max, {lex: true, not: even}), function (err) {
     t.error(err, 'no error from batch()')
 
     var pending = 3
-    var cb = function(err) {
+    var cb = function (err) {
       t.error(err, 'no error from end()')
       if (--pending === 0) done()
     }
@@ -211,20 +218,20 @@ make('iterator seek lands on or after target', function(db, t, done) {
       ite.seek(lexi.pack(i, 'hex'))
       ite.next(function (err, key, value) {
         var expected = even(i) ? i+1 : i
-        t.is(value && value.toString(), ''+expected, 'k'+expected)
+        t.equal(value.toString(), '' + expected, 'k' + expected)
         loop(ite, i+step, done)
       })
     }
   })
 })
 
-make('iterator reverse seek lands on or before target', function(db, t, done) {
+make('iterator reverse seek lands on or before target', function (db, t, done) {
   var max = 100, step = 15
-  db.batch(pairs(max, {lex: true, not: even}), function(err){
+  db.batch(pairs(max, {lex: true, not: even}), function (err) {
     t.error(err, 'no error from batch()')
 
     var pending = 3
-    var cb = function(err) {
+    var cb = function (err) {
       t.error(err, 'no error from end()')
       if (--pending === 0) done()
     }
@@ -238,100 +245,83 @@ make('iterator reverse seek lands on or before target', function(db, t, done) {
       ite.seek(lexi.pack(i, 'hex'))
       ite.next(function (err, key, value) {
         var expected = even(i) ? i-1 : i
-        t.is(value && value.toString(), ''+expected, 'k'+expected)
+        t.equal(value.toString(), '' + expected, 'k' + expected)
         loop(ite, i-step, done)
       })
     }
   })
 })
 
-make('iterator seek stays in range', function(db, t, done) {
-  db.batch(pairs(10), function(err){
+make('iterator seek stays in range', function (db, t, done) {
+  db.batch(pairs(10), function (err) {
     t.error(err, 'no error from batch()')
 
     var pending = 0
 
-    expect({ gt: 5 }, 4, undefined)
-    expect({ gt: 5 }, 5, undefined)
-    expect({ gt: 5 }, 6, 6)
+    expect({ gt: '5' }, '4', undefined)
+    expect({ gt: '5' }, '5', undefined)
+    expect({ gt: '5' }, '6', '6')
 
-    expect({ gte: 5 }, 4, undefined)
-    expect({ gte: 5 }, 5, 5)
-    expect({ gte: 5 }, 6, 6)
+    expect({ gte: '5' }, '4', undefined)
+    expect({ gte: '5' }, '5', '5')
+    expect({ gte: '5' }, '6', '6')
 
-    expect({ lt: 5 }, 4, 4)
-    expect({ lt: 5 }, 5, undefined)
-    expect({ lt: 5 }, 6, undefined)
+    expect({ lt: '5' }, '4', '4')
+    expect({ lt: '5' }, '5', undefined)
+    expect({ lt: '5' }, '6', undefined)
 
-    expect({ lt: 5, reverse: true }, 4, 4)
-    expect({ lt: 5, reverse: true }, 5, undefined)
-    expect({ lt: 5, reverse: true }, 6, undefined)
+    expect({ lt: '5', reverse: true }, '4', '4')
+    expect({ lt: '5', reverse: true }, '5', undefined)
+    expect({ lt: '5', reverse: true }, '6', undefined)
 
-    expect({ lte: 5, reverse: true }, 4, 4)
-    expect({ lte: 5, reverse: true }, 5, 5)
-    expect({ lte: 5, reverse: true }, 6, undefined)
+    expect({ lte: '5', reverse: true }, '4', '4')
+    expect({ lte: '5', reverse: true }, '5', '5')
+    expect({ lte: '5', reverse: true }, '6', undefined)
 
-    expect({ gt: 5, reverse: true }, 4, undefined)
-    expect({ gt: 5, reverse: true }, 5, undefined)
-    expect({ gt: 5, reverse: true }, 6, 6)
+    expect({ gt: '5', reverse: true }, '4', undefined)
+    expect({ gt: '5', reverse: true }, '5', undefined)
+    expect({ gt: '5', reverse: true }, '6', '6')
 
-    function expect(range, target, expected) {
-      var ite1 = create(range)
-      ite1.seek(''+target)
-      ite1.next(function(err, key, value) {
-        verify('initial seek', ite1, err, key, value)
-      })
+    function expect (range, target, expected) {
+      pending++
+      range.highWaterMark = 1
 
-      var ite2 = create(range)
-      ite2.next(function(err, key, value) {
-        t.error(err, 'no error from next()')
-        t.ok(key, 'has key')
-        ite2.seek(''+target)
-        ite2.next(function(err, key, value) {
-          verify('secondary seek', ite2, err, key, value)
-        })
-      })
+      var ite = db.iterator(range)
 
-      function verify(name, ite, err, key, value) {
+      ite.seek(target)
+      ite.next(function (err, key, value) {
         t.error(err, 'no error from next()')
 
-        var tpl = '%s(%d) on %s yields %s'
-        var msg = util.format(tpl, name, target, util.inspect(range), expected)
+        var tpl = 'seek(%s) on %s yields %s'
+        var msg = util.format(tpl, target, util.inspect(range), expected)
 
-        t.is(value ? value.toString() : ''+value, ''+expected, msg)
+        if (expected === undefined)
+          t.equal(value, undefined, msg)
+        else
+          t.equal(value.toString(), expected, msg)
 
-        ite.end(function end(err) {
+        ite.end(function (err) {
           t.error(err, 'no error from end()')
           if (!--pending) done()
         })
-      }
-    }
-
-    function create(range) {
-      pending++
-      var opts = { highWaterMark: 1, reverse: !!range.reverse }
-      if ('gt' in range) opts.gt = ''+range.gt
-      if ('gte' in range) opts.gte = ''+range.gte
-      if ('lt' in range) opts.lt = ''+range.lt
-      if ('lte' in range) opts.lte = ''+range.lte
-      return db.iterator(opts)
+      })
     }
   })
 })
 
-function pairs(length, opts) {
+function pairs (length, opts) {
   opts = opts || {}
-  return iota(length).filter(not(opts.not)).map(function(k){
-    var key = opts.lex ? lexi.pack(k, 'hex') : ''+k
-    return { type: opts.type || 'put', key: key, value: ''+k }
+  return iota(length).filter(not(opts.not)).map(function (k) {
+    var key = opts.lex ? lexi.pack(k, 'hex') : '' + k
+    return { type: 'put', key: key, value: '' + k }
   })
 }
 
-function not(n) {
-  if (typeof n === 'function') return function(k) { return !n(k) }
-  return function(k) { return k !== n }
+function not (n) {
+  if (typeof n === 'function') return function (k) { return !n(k) }
+  return function (k) { return k !== n }
 }
 
-function even(n) {
+function even (n) {
   return n % 2 === 0
 }
