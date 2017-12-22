6fe4cb4bfb586eb7e1bcb1eaa0f139099c207bc4
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-12-22 14:30:50 +0100

    rename ranges-test.js iterator-range-test.js

--- test/iterator-range-test.js
+++ test/iterator-range-test.js
@@ -1,6 +1,6 @@
 const test       = require('tape')
     , testCommon = require('abstract-leveldown/testCommon')
     , leveldown  = require('../')
-    , abstract   = require('abstract-leveldown/abstract/ranges-test')
+    , abstract   = require('abstract-leveldown/abstract/iterator-range-test')
 
 abstract.all(leveldown, test, testCommon)
