ef6a338163664474e9a3caff50ef19fd901f9bc8
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-12-25 01:21:05 +0100

    :fire: remove testBuffer

--- test/put-get-del-test.js
+++ test/put-get-del-test.js
@@ -1,8 +1,5 @@
 const test       = require('tape')
     , leveldown  = require('..')
-    , fs         = require('fs')
-    , path       = require('path')
-    , testBuffer = fs.readFileSync(path.join(__dirname, 'data/testdata.bin'))
     , abstract   = require('abstract-leveldown/abstract/put-get-del-test')
 
-abstract.all(leveldown, test, testBuffer)
+abstract.all(leveldown, test)
