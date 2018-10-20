9af26659884fe182219445c196017ef9960ce1b2
Vincent Weevers <mail@vincentweevers.nl>
2018-10-20 19:20:49 +0200

    handle errors in compactRange test

--- test/compact-range-test.js
+++ test/compact-range-test.js
@@ -15,16 +15,32 @@ test('test compactRange() frees disk space after key deletion', function (t) {
   var key2 = '000001'
   var val1 = Buffer.allocUnsafe(64).fill(1)
   var val2 = Buffer.allocUnsafe(64).fill(1)
-  db.put(key1, val1, function () {
-    db.put(key2, val2, function () {
-      db.compactRange(key1, key2, function () {
+
+  // TODO: use batch()
+  db.put(key1, val1, function (err) {
+    t.ifError(err, 'no put1 error')
+
+    db.put(key2, val2, function (err) {
+      t.ifError(err, 'no put2 error')
+
+      db.compactRange(key1, key2, function (err) {
+        t.ifError(err, 'no compactRange1 error')
+
         db.approximateSize('0', 'z', function (err, sizeAfterPuts) {
-          t.error(err, 'no error')
-          db.del(key1, function () {
-            db.del(key2, function () {
-              db.compactRange(key1, key2, function () {
+          t.error(err, 'no approximateSize1 error')
+
+          // TODO: use batch()
+          db.del(key1, function (err) {
+            t.ifError(err, 'no del1 error')
+
+            db.del(key2, function (err) {
+              t.ifError(err, 'no del2 error')
+
+              db.compactRange(key1, key2, function (err) {
+                t.ifError(err, 'no compactRange2 error')
+
                 db.approximateSize('0', 'z', function (err, sizeAfterCompact) {
-                  t.error(err, 'no error')
+                  t.error(err, 'no approximateSize2 error')
                   t.ok(sizeAfterCompact < sizeAfterPuts)
                   t.end()
                 })
