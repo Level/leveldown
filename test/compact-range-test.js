c604cc443e5fb3adb97285e68a58bb78c549ee72
Vincent Weevers <mail@vincentweevers.nl>
2018-11-24 22:49:27 +0200

    Serialize compact range options (#517)
    * handle errors in compactRange test
    
    * use batch() in compactRange test
    
    * serialize compactRange() start and end
    
    * copy type checks from approximateSize to compactRange

--- test/compact-range-test.js
+++ test/compact-range-test.js
@@ -15,25 +15,47 @@ test('test compactRange() frees disk space after key deletion', function (t) {
   var key2 = '000001'
   var val1 = Buffer.allocUnsafe(64).fill(1)
   var val2 = Buffer.allocUnsafe(64).fill(1)
-  db.put(key1, val1, function () {
-    db.put(key2, val2, function () {
-      db.compactRange(key1, key2, function () {
-        db.approximateSize('0', 'z', function (err, sizeAfterPuts) {
-          t.error(err, 'no error')
-          db.del(key1, function () {
-            db.del(key2, function () {
-              db.compactRange(key1, key2, function () {
-                db.approximateSize('0', 'z', function (err, sizeAfterCompact) {
-                  t.error(err, 'no error')
-                  t.ok(sizeAfterCompact < sizeAfterPuts)
-                  t.end()
-                })
-              })
+
+  db.batch().put(key1, val1).put(key2, val2).write(function (err) {
+    t.ifError(err, 'no batch put error')
+
+    db.compactRange(key1, key2, function (err) {
+      t.ifError(err, 'no compactRange1 error')
+
+      db.approximateSize('0', 'z', function (err, sizeAfterPuts) {
+        t.error(err, 'no approximateSize1 error')
+
+        db.batch().del(key1).del(key2).write(function (err) {
+          t.ifError(err, 'no batch del error')
+
+          db.compactRange(key1, key2, function (err) {
+            t.ifError(err, 'no compactRange2 error')
+
+            db.approximateSize('0', 'z', function (err, sizeAfterCompact) {
+              t.error(err, 'no approximateSize2 error')
+              t.ok(sizeAfterCompact < sizeAfterPuts)
+              t.end()
             })
           })
         })
       })
     })
+  })
+})
+
+test('test compactRange() serializes start and end', function (t) {
+  t.plan(3)
+
+  var clone = Object.create(db)
+  var count = 0
+
+  clone._serializeKey = function (key) {
+    t.is(key, count++)
+    return db._serializeKey(key)
+  }
+
+  clone.compactRange(0, 1, function (err) {
+    t.ifError(err, 'no compactRange error')
   })
 })
 
