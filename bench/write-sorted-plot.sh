0cf69aec5b0e256b4bbc8302ca9f81130f89a9d9
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-12-15 12:34:24 +0100

    Bench tweaks (#546)
    * .benchmark not used, always clean db
    
    * Make write-random same style as db-bench
    
    * Make write-sorted same style as db-bench

--- bench/write-sorted-plot.sh
+++ bench/write-sorted-plot.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 
+num_writes=10M
+num_files=$#
+platform=$(node -p process.platform)
+arch=$(node -p process.arch)
+node=$(node -p process.version)
+png_suffix=$(date '+%Y%m%d-%H.%M.%S')
+
 gnuplot <<EOF
   reset
-  set terminal png size 1920, 1080
-  set output "write_sorted_times.png"
+  set terminal pngcairo truecolor enhanced font "Ubuntu Mono,13" size 1920, 1080 background rgb "#1b1b1b"
+  set output "bench/write-sorted-$png_suffix.png"
   set datafile separator ','
 
-  #set yrange [0:0.6]
+  set autoscale y
 
-  set xlabel "Seconds"
-  set ylabel "Milliseconds per write"
+  set xlabel "Time (seconds)" tc rgb "#777777"
+  set ylabel "Milliseconds/write" tc rgb "#888888"
 
-  set title "1.3G / 10,000,000 writes"
-  set key below
+  set key outside tc rgb "#777777"
+  set border lc rgb "#777777"
   set grid
 
-  plot "write_sorted_times_g.csv" using (\$1/1000):(\$2/1000000) title "Google LevelDB" lc rgb "red" lt 7 ps 0.3, \
-       "write_sorted_times_h.csv" using (\$1/1000):(\$2/1000000) title "HyperDex LevelDB" lc rgb "green" lt 7 ps 0.3, \
-       "write_sorted_times_b.csv" using (\$1/1000):(\$2/1000000) title "Basho LevelDB" lc rgb "blue" lt 7 ps 0.3
+  # To plot more than 5 files, add more line styles
+  set style line 1 lt 7 ps 0.8 lc rgb "#00FFFF"
+  set style line 2 lt 7 ps 0.8 lc rgb "#D84797"
+  set style line 3 lt 7 ps 0.8 lc rgb "#23CE6B"
+  set style line 4 lt 7 ps 0.8 lc rgb "#F5B700"
+  set style line 5 lt 7 ps 0.8 lc rgb "#731DD8"
+
+  set title "leveldown write-sorted, $num_writes writes, $platform $arch, node $node" tc rgb "#cccccc"
+
+  filename(n) = word("$@", n)
+  basename(n) = substr(word("$@", n), 0, strstrt(word("$@", n), ".csv") - 1)
+
+  plot for [i=1:$num_files] filename(i) using (\$1/1000):(\$2/1000000) title basename(i) ls i axes x1y1
 
 EOF
