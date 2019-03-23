0b8636f79d6fb6f58bf3ee51c022a235c9c95b55
Vincent Weevers <mail@vincentweevers.nl>
2019-03-23 16:56:30 +0100

    Update comments in cleanup-hanging-iterators-test

--- test/cleanup-hanging-iterators-test.js
+++ test/cleanup-hanging-iterators-test.js
@@ -1,8 +1,8 @@
 const makeTest = require('./make')
 const repeats = 200
 
-makeTest('test ended iterator', function (db, t, done) {
-  // First test normal and proper usage: calling it.end() before db.close()
+makeTest('test legacy ended iterator', function (db, t, done) {
+  // First test the old proper usage: calling it.end() before db.close().
   var it = db.iterator({ keyAsBuffer: false, valueAsBuffer: false })
 
   it.next(function (err, key, value) {
@@ -17,7 +17,7 @@ makeTest('test ended iterator', function (db, t, done) {
 })
 
 makeTest('test likely-ended iterator', function (db, t, done) {
-  // Test improper usage: not calling it.end() before db.close(). Cleanup of the
+  // Test simpler usage: not calling it.end() before db.close(). Cleanup of the
   // database will crash Node if not handled properly.
   var it = db.iterator({ keyAsBuffer: false, valueAsBuffer: false })
 
@@ -49,7 +49,7 @@ makeTest('test non-ended iterator', function (db, t, done) {
 
 makeTest('test multiple likely-ended iterators', function (db, t, done) {
   // Same as the test above but repeated and with an extra iterator that is not
-  // nexting, which means its EndWorker will be executed almost immediately.
+  // nexting, which follows a different (faster) code path.
   for (let i = 0; i < repeats; i++) {
     db.iterator()
     db.iterator().next(function () {})
@@ -70,7 +70,7 @@ makeTest('test multiple non-ended iterators', function (db, t, done) {
 
 global.gc && makeTest('test multiple non-ended iterators with forced gc', function (db, t, done) {
   // Same as the test above but with forced GC, to test that the lifespan of an
-  // iterator is tied to *both* its JS object and whether the iterator was ended.
+  // iterator is tied to *both* its JS object and whether it's busy (nexting).
   for (let i = 0; i < repeats; i++) {
     db.iterator({ highWaterMark: 0 })
     db.iterator({ highWaterMark: 0 }).next(function () {})
@@ -83,6 +83,7 @@ global.gc && makeTest('test multiple non-ended iterators with forced gc', functi
 })
 
 global.gc && makeTest('test multiple iterators with forced gc in next()', function (db, t, done) {
+  // Simulate GC in between NextWorker::DoComplete() and ::DoFinally().
   for (let i = 0; i < 10; i++) {
     db.iterator({ highWaterMark: 0 }).next(function () {
       global.gc()
@@ -92,8 +93,8 @@ global.gc && makeTest('test multiple iterators with forced gc in next()', functi
   setImmediate(done)
 })
 
-makeTest('test ending iterators', function (db, t, done) {
-  // At least one end() should be in progress when we try to close the db.
+makeTest('test legacy ended iterators', function (db, t, done) {
+  // This test doesn't have added value anymore.
   var it1 = db.iterator().next(function () {
     it1.end(function () {})
   })
