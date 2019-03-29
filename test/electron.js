88ca973cacf596f00601d927b9ad367a51ec2865
Vincent Weevers <mail@vincentweevers.nl>
2019-03-29 19:49:37 +0100

    Test Electron in Travis and AppVeyor

---
+++ test/electron.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const tape = require('tape')
+const electron = require('electron')
+const path = require('path')
+const glob = require('glob')
+const app = electron.app
+
+process.on('uncaughtException', function (err) {
+  console.error(err)
+  app.exit(1)
+})
+
+app.on('ready', function () {
+  tape.onFinish(() => app.quit())
+  tape.onFailure(() => app.exit(1))
+
+  for (let file of glob.sync('test/*-test.js')) {
+    require(path.resolve('.', file))
+  }
+})
