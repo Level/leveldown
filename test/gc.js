e0f51459c7fb5ceb64bd88ae67d5018176940b87
Vincent Weevers <mail@vincentweevers.nl>
2019-10-22 01:00:21 +0300

    Fix test-gc script (#691)

---
+++ test/gc.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// A special entry point to run certain tests with forced gc.
+
+if (!global.gc) {
+  console.error('Run with node --expose-gc')
+  process.exit(1)
+}
+
+require('./cleanup-hanging-iterators-test')
+require('./iterator-gc-test')
+require('./chained-batch-gc-test')
